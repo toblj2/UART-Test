28e608014199b5f26d41fc6caf5217eef5d733ec
toblj2 <toblj2@bfh.ch>
2018-01-03 03:20:30 +0100

    Changes tested with Carme.

--- src/main.c
+++ src/main.c
@@ -106,22 +106,47 @@ int main(void)
 		while ((USART_GetFlagStatus(USART1, USART_FLAG_RXNE ) == RESET) &&
 			 (deadloopprev < 10000))
 		{
-		  deadloopprev++;
+			deadloopprev++;
 		}
 		deadloopprev = 0;
 		while ((USART_GetFlagStatus(USART1, USART_FLAG_RXNE ) == SET) &&
 			 (deadloopprev < 10000))
 		{
-		  for (int i = 0; i<=count; i++ )
-		  {
-			  uint16_t data_received;
-			  data_received = USART_ReceiveData(USART1);
-			  if (data_received <= 0xFF)
-			  {
-				  pc[i] = data_received;
-			  }
-		  }
-		  deadloopprev++;
+			uint8_t NMEA_start = 0;
+			uint8_t sectionCount = 0;
+			for (int i = 0; (i<count)&&(sectionCount<7); i++ )
+			{
+				uint16_t data_received;
+				if (USART_GetFlagStatus(USART1, USART_FLAG_RXNE ) == SET)
+				{
+					data_received = USART_ReceiveData(USART1);
+					USART_ClearFlag(USART1, USART_FLAG_RXNE);
+					if (data_received <= 0xFF)
+					{
+						switch (data_received)
+						{
+							case '$':
+								if (i<2) i=0;
+								NMEA_start = 1;
+								sectionCount++;
+								break;
+							case '\r':
+								NMEA_start = 0;
+								break;
+							case '\n':
+								NMEA_start = 0;
+								break;
+							default:
+								break;
+						}
+						if(NMEA_start)
+							NMEA_string[i] = data_received;
+						else i--;
+					}
+				}
+				else if (NMEA_string[i] == 0 && i>0) i--;
+			}
+			deadloopprev++;
 		}
 		deadloopprev = 0;
 		LCD_DisplayWholeString(0, pc);
