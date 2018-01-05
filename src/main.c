fedfb6272c5a32edde742d65e78527b4e3655814
toblj2 <33353143+toblj2@users.noreply.github.com>
2018-01-05 22:19:15 +0100

    Update main.c
    forgot to remove polling code

--- src/main.c
+++ src/main.c
@@ -88,53 +88,6 @@ int main(void)
 	/* Infinite loop */
 	while (1)
 	{
-		int deadloopprev;
-		while ((USART_GetFlagStatus(USART1, USART_FLAG_RXNE ) == RESET) &&
-			 (deadloopprev < 10000))
-		{
-			deadloopprev++;
-		}
-		deadloopprev = 0;
-		while ((USART_GetFlagStatus(USART1, USART_FLAG_RXNE ) == SET) &&
-			 (deadloopprev < 10000))
-		{
-			uint8_t NMEA_start = 0;
-			uint8_t sectionCount = 0;
-			for (int i = 0; (i<NMEA_stringlength)&&(sectionCount<7); i++ )
-			{
-				uint16_t data_received;
-				if (USART_GetFlagStatus(USART1, USART_FLAG_RXNE ) == SET)
-				{
-					data_received = USART_ReceiveData(USART1);
-					USART_ClearFlag(USART1, USART_FLAG_RXNE);
-					if (data_received <= 0xFF)
-					{
-						switch (data_received)
-						{
-							case '$':
-								if (i<2) i=0;
-								NMEA_start = 1;
-								sectionCount++;
-								break;
-							case '\r':
-								NMEA_start = 0;
-								break;
-							case '\n':
-								NMEA_start = 0;
-								break;
-							default:
-								break;
-						}
-						if(NMEA_start)
-							NMEA_string[i] = data_received;
-						else i--;
-					}
-				}
-				else if (NMEA_string[i] == 0 && i>0) i--;
-			}
-			deadloopprev++;
-		}
-		deadloopprev = 0;
 		NVIC_DisableIRQ(USART1_IRQn);
 		LCD_DisplayWholeString(0, NMEA_string);
 		NVIC_EnableIRQ(USART1_IRQn);
